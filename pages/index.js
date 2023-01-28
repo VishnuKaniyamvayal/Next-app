642ae8cabf3709f1d1f24fa9175a6791204a5181
Vishnu <vishnujishnu0987@gmail.com>
2023-01-27 16:02:54 -0800

    minor bugs fixed

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,10 @@ import Link from "next/link"
 
 
 export default function Home() {
-  let pnglink='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/800px-Nextjs-logo.svg.png'
   let intro = "Welcome to the Next.js Foundations course! This beginner-friendly, example-led course will guide you through the prerequisite knowledge for Next.js. You will build a simple project step-by-step; starting with a JavaScript application, then migrating it to React and Next.js."
   return (
    <div className="">
-    <Image className='max-w-xs mx-auto md:max-w-sm' src={pnglink} alt="NEXT JS" />
+    <img className='max-w-xs mx-auto md:max-w-sm' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/800px-Nextjs-logo.svg.png" alt="NEXT JS" />
     <div className='max-w-xl mx-auto'>
     <h1 className='font-semibold pl-5 text-[20px]'>Introduction:</h1>
     <p className=' text-left mx-auto px-5 lg:text-xl pt-4'>{intro}</p>
